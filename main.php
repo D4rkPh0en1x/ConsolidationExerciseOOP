0e07712eb2a2e94c084c1bd1422ed3819aa5bc4c
D4rkPh0en1x <frank@datateam.lu>
2018-03-07 11:38:21 +0100

    update

--- main.php
+++ main.php
@@ -1,3 +1,21 @@
 <?php
 
+spl_autoload_register(
+    function($namespace){
+        //$filename = sprintf('%s\%s.php', __DIR__, str_replace('\\', DIRECTORY_SEPARATOR, $namespace));
+        //var_dump ($filename);
+        $filename = $namespace . '.php';
+        $filename = str_replace('\\', DIRECTORY_SEPARATOR, $filename);
+        $filename = __DIR__ . DIRECTORY_SEPARATOR . $filename;
+        
+        if (is_file($filename)) {
+            require_once $filename;
+        }
+        
+    }
+    );
+
+use lib\Normalizer;
+use lib\Serializer;
+use lib\Metadata;
 
